d81ea25c5af06e8cca0d8246182050efd2e55f27
ZhmAA <azhm@ukr.net>
2015-12-02 23:29:02 +0200

    end visual and functional part

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,18 @@
-jQuery(document).ready(function() {
-	jQuery('body').append('<div id="top"></div>');
-	jQuery("#top").click(function () {
-		jQuery("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
-});
+ jQuery(document).ready(function() {
+ jQuery('body').append('<div id="top"></div>');
+ jQuery("#top").click(function () {
+  jQuery("body, html").animate({
+   scrollTop: 0
+  }, 800);
+  return false;
+ });
+ jQuery('.main-nav').find('a').click(function() {
+  var it = jQuery(this)
+   .attr('href')
+   .substring(2);
+  if(jQuery.trim(it)!=''){
+   jQuery('html,body').animate({scrollTop:jQuery('#'+it).offset().top},800);
+   return false;
+  }
+ });
+}); 
